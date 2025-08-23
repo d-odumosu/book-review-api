55c5c4c321461b3721435c95bbd2ac251ae4540f
d-odumosu <dt.odumosu@gmail.com>
2025-08-23 19:16:25 +0100

    completed the user.java class

---
+++ src/main/java/com/me/bookreviewapi/model/Comment.java
@@ -0,0 +1,5 @@
+package com.me.bookreviewapi.model;
+
+public class Comment extends Feedback{
+
+}
---
+++ src/main/java/com/me/bookreviewapi/model/User.java
@@ -0,0 +1,52 @@
+package com.me.bookreviewapi.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String email;
+
+    public User(String firstName, String lastName, String userName, String email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.email =email;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = lastName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
